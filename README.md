1504ab14a9b48e059e0f8d5ab7624d1ac5bfc02a
jain-sonika <sonikajain0002@gmail.com>
2024-03-27 20:29:44 +0530

    completed

---
+++ README.md
@@ -0,0 +1,262 @@
+
+## A clean, beautiful and responsive portfolio template for Developers!
+
+
+Just change `src/portfolio.js` to get your personal portfolio. Customize portfolio theme by using your own color scheme globally in the  `src/_globalColor.scss` file. Feel free to use it as-is or personalize it as much as you want.
+
+
+## Table of Contents
+- [Sections](#sections)
+- [Getting Started](#getting-started)
+- [How to Use](#how-to-use)
+- [Linking portfolio to GitHub](#linking-portfolio-to-github)
+- [Linking blogs section to Medium](#linking-blogs-section-to-medium)
+- [Change and Customize](#change-and-customize-every-section-according-to-your-need)
+- [Deployment](#deployment)
+- [Technologies Used](#technologies-used)
+- [Illustrations](#illustrations)
+- [For the Future](#for-the-future)
+- [Contributors](#project-maintainers)
+
+## Portfolio Sections
+✔️ Summary and About me\
+✔️ Skills\
+✔️ Education\
+✔️ Work Experience\
+✔️ Open Source Projects Connected with GitHub\
+✔️ Big Projects\
+✔️ Achievements And Certifications 🏆\
+✔️ Blogs\
+✔️ Talks\
+✔️ Podcast\
+✔️ Contact me\
+✔️ Twitter Timeline\
+✔️ GitHub Profile
+
+To view a live example, **[click here](https://6602fef45c335e65f93d9b92--loquacious-truffle-4130b7.netlify.app/)**.
+
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+You'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer or use [Docker](https://www.docker.com/products/docker-desktop).
+
+```
+node@v10.16.0 or higher
+npm@6.9.0 or higher
+git@2.17.1 or higher
+```
+### Docker Commands
+
+```
+1) BUILD IMAGE : docker build -t developerfolio:latest .
+2) RUN IMAGE: docker run -t -p 3000:3000 developerfolio:latest
+```
+
+
+## How To Use 
+
+From your command line, clone and run developerFolio:
+
+```bash
+# Clone this repository
+git clone https://github.com/jain-sonika/Portfolio.git
+
+# Go into the repository
+cd developerFolio
+
+# Setup default environment variables
+
+# For Linux
+cp env.example .env
+# For Windows
+copy env.example .env
+
+# Install dependencies
+npm install
+
+# Start a local dev server
+npm start
+```
+
+## Linking Portfolio to GitHub
+
+Generate a classic GitHub personal access token following these [instructions](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token#creating-a-personal-access-token-classic) (make sure you don't select any scope just generate a simple token). If you are using [GitHub Actions](#configuring-github-actions-recommended) to deploy your portfolio you can skip this section.
+
+1. Create a file called .env in the root directory of your project (if not done already in section: [How To Use](#how-to-use))
+
+Note: Configuring environment variables before deploying your portfolio is highly recommended as some components depend on API data. 
+
+```bash
+- DeveloperFolio
+  - node_modules
+  - public
+  - src
+  - .env         <-- create it here
+  - env.example  <-- this is the base file
+  - .gitignore
+  - package-lock.json
+  - package.json
+```
+
+2. Inside the .env file, add key `REACT_APP_GITHUB_TOKEN` and assign your GitHub token like this, also add your username as `GITHUB_USERNAME`
+
+```env
+// .env
+REACT_APP_GITHUB_TOKEN = "YOUR GITHUB TOKEN HERE"
+GITHUB_USERNAME = "YOUR GITHUB USERNAME"
+USE_GITHUB_DATA = "true"
+```
+
+Set `showGithubProfile` to true or false to show Contact Profile using GitHub, defaults to false.
+
+**Warning:** Treat your tokens like passwords and keep them secret. When working with the API, use tokens as environment variables instead of hardcoding them into your programs.
+
+Note: Open Source Projects section only show pinned items of your GitHub.
+If you are seeing something as shown below, follow these [instructions](https://docs.github.com/en/enterprise/2.13/user/articles/pinning-items-to-your-profile).
+
+
+
+If the above solution still doesn't work, visit the [wiki page] https://github.com/jain-sonika/Portfolio.git
+
+## Linking blogs section to Medium
+
+Optionally, you can link the blogs section to your medium user account:
+
+* Inside the .env file, add key `MEDIUM_USERNAME` and assign your Medium username
+
+```env
+// .env
+MEDIUM_USERNAME = "YOUR MEDIUM USERNAME"
+```
+
+* For Github Action, change the environment variable `MEDIUM_USERNAME` in `.github/workflows/deploy.yml`
+
+Set `displayMediumBlogs` to true or false in portofolio.js to display fetched Medium blogs, defaults to true.
+
+## Change and customize every section according to your need.
+
+#### Personalize page content in `/src/portfolio.js` & modify it as per your need. You will also need to modify `index.html` to change the title and metadata to provide accurate SEO for your personal portfolio.
+
+```javascript
+/* Change this file to get your Personal Porfolio */
+
+const greeting = {
+  /* Your Summary And Greeting Section */
+  title: "Hi all I'm Sonika",
+  subTitle: emoji("A passionate Full Stack Software Developer 🚀"),
+  resumeLink: "https://drive.google.com/file/d/1eAZ6fC8FvqmAPKSdP5bCcsb0g0OnA_Kq/view?usp=sharing"
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/jain-sonika",
+  linkedin: "https://www.linkedin.com/in/sonika-jain-128b2a25a/",
+  gmail: "sonikajain0002@gmail.com",
+};
+
+
+const skillsSection = { .... }
+
+const techStack = { .... }
+
+const workExperience = { .... }
+
+const openSource = { .... }
+
+const bigProjects = { .... }
+
+const achievementSection = { .... }
+
+const blogSection = { .... }
+
+const contactInfo = { .... }
+
+const twitterDetails = { ... }
+
+```
+
+#### Using Emojis
+
+For adding emoji 😃 into the texts in `Portfolio.js`, use the `emoji()` function and pass the text you need as an argument. This would help in keeping emojis compatible across different browsers and platforms.
+
+#### Customize Lottie Animations
+
+You can choose a Lottie and download it in json format from from sites like [this](https://lottiefiles.com/). In `src/assets/lottie`, replace the Lottie json file you want to alter with the same file name. If you want to change the Lottie options, go to `src/components/displayLottie/DisplayLottie.js` and change the `defaultOptions` object, you can refer [lottie-react docs](https://www.npmjs.com/package/lottie-react) for more info on the `defaultOptions` object.
+
+#### Adding Twitter Time line to your Page
+Insert your Twitter username in `portfolio.js` to show your recent activity on your page.
+
+```javascript
+const twitterDetails = {
+  userName : "Your Twitter Username"
+};
+```
+Note: Don't use `@` symbol when adding username.
+
+## Deployment
+When you are done with the setup, you should host your website online.
+We highly recommend to read through the [Deploying on GitHub Pages](https://create-react-app.dev/docs/deployment/#github-pages) docs for React.
+
+#### Configuring GitHub Actions (Recommended)
+First you should enable, GitHub Actions for the repository you use.
+
+The Profile and the Repository information from GitHub is only created at the time of deploy and the site needs to be redeployed if those information needs to be updated. So, a configurable [CRON Job](https://docs.github.com/en/actions/reference/events-that-trigger-workflows#scheduled-events) is setup which deploys your site every week, so that once you update your profile on GitHub it is shown on your portfolio. You can also trigger it manually using `workflow_dispatch` event, see [this guide](https://github.blog/changelog/2020-07-06-github-actions-manual-triggers-with-workflow_dispatch) on how to do that.
+
+- When you are done with the configuration, we highly recommend to read through the [GitHub Actions Configuring a workflow](https://docs.github.com/en/actions/configuring-and-managing-workflows/configuring-a-workflow) docs.
+
+#### Deploying to GitHub Pages
+
+This section guides you to deploy your portfolio on GitHub pages.
+
+- Navigate to `package.json` and enter your domain name instead of `https://developerfolio.js.org/` in `homepage` variable. For example, if you want your site to be `https://<your-username>.github.io/developerFolio`, add the same to the homepage section of `package.json`.
+
+- In short you can also add `/devloperFolio` to `package.json` as both are exactly same. Upon doing so, you tell `create-react-app` to add the path assets accordingly.
+
+- Optionally, configure the domain. You can configure a custom domain with GitHub Pages by adding a `CNAME` file to the `public/` folder.
+
+- Follow through the guide to setup GitHub pages from the official CRA docs [here](https://create-react-app.dev/docs/deployment/#github-pages).
+
+#### Deploying to Netlify
+
+You could also host directly with Netlify by linking your own repository.
+
+[![Deploy To Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/saadpasta/developerFolio)
+
+For more information, read [hosting on Netlify](https://create-react-app.dev/docs/deployment/#netlify).
+
+
+## Technologies Used 
+
+- [React](https://reactjs.org/)
+- [graphql](https://graphql.org/)
+- [apollo-boost](https://www.apollographql.com/docs/react/get-started/)
+- [react-easy-emoji](https://github.com/appfigures/react-easy-emoji)
+- [react-headroom](https://github.com/KyleAMathews/react-headroom)
+
+
+## Illustrations
+- [UnDraw](https://undraw.co/illustrations)
+- [Lottie by Oblikweare](https://lottiefiles.com/oblikweare)
+
+
+## For the Future
+If you can help us with these. Please don't hesitate to open a [pull request](https://github.com/jain-sonika/developerFolio/pulls).
+
+- Connect with LinkedIn to get Summary, Skills, Education and Experience
+
+- Move to Gatsby
+
+- Add More Sections
+
+
+
+
+
+<!-- markdownlint-restore -->
+<!-- prettier-ignore-end -->
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+---
---
+++ src/portfolio.js
@@ -0,0 +1,398 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+
+// Splash Screen
+
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
+};
+
+// Summary And Greeting Section
+
+const illustration = {
+  animated: true // Set to false to use static SVG
+};
+
+const greeting = {
+  username: "Sonika Jain",
+  title: "Hi all, I'm Sonika",
+  subTitle: emoji(
+    "A passionate Full Stack Software Developer 🚀 having an experience of building Web with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1eAZ6fC8FvqmAPKSdP5bCcsb0g0OnA_Kq/view?usp=sharing", // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/jain-sonika",
+  linkedin: "https://www.linkedin.com/in/sonika-jain-128b2a25a/",
+  gmail: "sonikajain0002@gmail.com",
+  // Instagram, Twitter and Kaggle are also supported in the links!
+  // To customize icons and social links, tweak src/components/SocialMedia
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for web applications"
+    ),
+    emoji("⚡ Progressive Web Applications"),
+    emoji(
+      "⚡ Develop java application will help to generate E-mail Id based on department"
+    )
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "mongo-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "json",
+      fontAwesomeClassname: "fas fa-code"
+    },
+    {
+      skillName: "java",
+      fontAwesomeClassname: "fab fa-java"
+    },
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "Dr APJ Abdul Kalam Technical University",
+      logo: require("./assets/images/harvardLogo.png"),
+      subHeader: "Bachlors in Computer Science",
+      duration: "July 2019 - June 2023",
+      desc: "Took courses about Software Engineering, Web Security, Operating Systems, ...",
+      descBullets: [
+        "Highly motivated and skilled Fresher Computer Science with a passion for developing innovative software solutions",
+      ]
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "90%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "70%"
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "60%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: false, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Software Engineer",
+      company: "Facebook",
+      companylogo: require("./assets/images/facebookLogo.png"),
+      date: "June 2018 – Present",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      descBullets: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+      ]
+    },
+    {
+      role: "Front-End Developer",
+      company: "Quora",
+      companylogo: require("./assets/images/quoraLogo.png"),
+      date: "May 2017 – May 2018",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      role: "Software Engineer Intern",
+      company: "Airbnb",
+      companylogo: require("./assets/images/airbnbLogo.png"),
+      date: "Jan 2015 – Sep 2015",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "My Projects",
+  display: true,
+  subtitle: "SOME OF MY DEVELOPEMENT GEMS ",
+  projects: [
+    {
+      image: require("./assets/images/saayaHealthLogo.webp"),
+      projectName: "Weather Forecasting",
+      projectDesc: "In Weather Forecasting user can search locations by city name and get the weather information for the next 5-6 days and 3 hour interval.",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://the-weather-forecasting.netlify.app/"
+        },
+        {
+          name: "GitHub",
+          url: "https://github.com/jain-sonika/Weather-Forecasting"
+        }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/news.webp"),
+      projectName: "News-App",
+      projectDesc: "This News App is powered by the GNews public API and provides users with the latest news articles from around the world.",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://yournewsdaily.netlify.app/"
+        },
+        {
+          name: "GitHub",
+          url: "https://github.com/jain-sonika/News-App"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/country.webp"),
+      projectName: "Country-Pages",
+      projectDesc: "Get Country Flag, Name , population, areas(km) , region , search counties and more . Get any country information regardless how you search",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://6601d055398f2b3adb77383a--brilliant-blini-afde62.netlify.app/"
+        },
+        {
+          name: "GitHub",
+          url: "https://github.com/jain-sonika/Country-Pages"
+        }
+      ]
+    },
+    {
+      image: require("./assets/images/email.webp"),
+      projectName: "Email-App",
+      projectDesc: "This application will help to generate E-mail Id based on department.Have get methods to display the name, email, and mailbox capacity",
+      footerLink: [
+        {
+          name: "Visit Website",
+          url: "https://github.com/jain-sonika/Projects/tree/Java-Projects/Email%20Application"
+        },
+        {
+          name: "GitHub",
+          url: "https://github.com/jain-sonika/Projects/tree/Java-Projects/Email%20Application"
+        }
+      ]
+    }
+  ], // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "HackerRank",
+      subtitle:
+        "Cleared the HakerRank Assesment for the various skills like javascript, React, HTML, CSS, etc.",
+      image: require("./assets/images/HackerRank.webp"),
+      footerLink: [
+        {
+          name: "Certificates",
+          url: "https://www.hackerrank.com/profile/sonikajain0002"
+        },
+      ]
+    },
+    {
+      title: "Virtual Experience Program",
+      subtitle:
+        "Partifipated and completed technology virtual experience program for Walmart, Deloitte, Accenture.",
+      image: require("./assets/images/Forage.webp"),
+      footerLink: [
+        {
+          name: "Certificates",
+          url: "https://drive.google.com/drive/folders/17JoUCfjYIrR-VU203T-26j2k8JYkcuzw"
+        }
+      ]
+    },
+
+    {
+      title: "Leetcode",
+      subtitle: "Solved various Data structures and algorithm question and earned several badges based on my performance",
+      image: require("./assets/images/leetcode.webp"),
+      footerLink: [
+        {
+          name: "Badges",
+          url: "https://leetcode.com/sonikajain0002/"
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+  displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
+  blogs: [
+    {
+      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  talks: [
+    {
+      title: "Build Actions For Google Assistant",
+      subtitle: "Codelab at GDG DevFest Karachi 2019",
+      slides_url: "https://bit.ly/saadpasta-slides",
+      event_url: "https://www.facebook.com/events/2339906106275053/"
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "+91-7398068988",
+  email_address: "sonikajain0002@gmail.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "twitter", //Replace "twitter" with your twitter username without @
+  display: false // Set true to display this section, defaults to false
+};
+
+const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  splashScreen,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+  isHireable
+};
